642c80c0689be7920dcb319ae382fe08072a7889
NiKhil GarakaPati <nikhilgarakapati2829@gmail.com>
2018-02-14 15:08:33 +0530

    Create solution.cpp

---
+++ Day 0-Hello- World/solution.cpp
@@ -0,0 +1,10 @@
+#include<iostream.h>
+using namespace std;
+int main() {
+
+    string inputString; 
+    getline(cin, inputString);
+    cout << "Hello, World." << endl;
+    cout << inputString << endl;
+    return 0;
+}
